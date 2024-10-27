8cb55a260e2a49337ccf5423eb0bb32d0bc724e8
saniyapadwal <saniyapadwal198@gmail.com>
2024-10-27 14:15:07 +0530

    My first maven commit

---
+++ src/main/java/com/dbit/App.java
@@ -0,0 +1,13 @@
+package com.dbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
